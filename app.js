38b4dd8d525b45ddbe3ccf2e9530f9bdd6d36eac
Munkhbold Enkhtur <apple.muugii@gmail.com>
2019-02-02 17:41:50 -0600

    simple node server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const parser = require('body-parser')
+const path = require('path')
+const app = express()
+
+//:1 middle wares
+app.use(parser.json())
+app.use(express.static(__dirname + '/templates/static/css/'))
+// endfold
+
+app.get('/design/', (req, res) => {
+  let template = '/templates/index.html'
+  template = path.join(__dirname + template)
+  res.sendFile(template)
+})
+app.listen(8080, () => console.log('listening on 8080'))
