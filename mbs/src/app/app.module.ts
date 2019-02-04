a8c2787b83c13d3a8707697a23ffb019731164e1
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-02-04 10:41:04 -0600

    saving snapshot to local var

--- mbs/src/app/app.module.ts
+++ mbs/src/app/app.module.ts
@@ -3,25 +3,21 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./routing";
 import { AppComponent } from "./app.component";
-import { QuestionComponent } from './student/question/question.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http'
-import { AdminLogin } from './admin/login'
+import { QuestionComponent } from "./student/question/question.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AdminLogin } from "./admin/login";
 import { AceEditorModule } from "ng2-ace-editor";
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		QuestionComponent,
-		AdminLogin
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		ReactiveFormsModule,
-		HttpClientModule,
-		AceEditorModule
-	],
+  declarations: [AppComponent, QuestionComponent, AdminLogin],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AceEditorModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
