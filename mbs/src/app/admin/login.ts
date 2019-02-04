07f5b9fa8dd6d4f1f933ab355c20775035ec35f8
Munkhbold Enkhtur <apple.muugii@gmail.com>
2019-02-03 21:22:39 -0600

    login form

---
+++ mbs/src/app/admin/login.ts
@@ -0,0 +1,56 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import {
+	FormGroup,
+	FormControl,
+	FormBuilder,
+	Validators,
+} from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+	encapsulation: ViewEncapsulation.None,
+	template: `
+	<form [formGroup]='loginForm' (ngSubmit)='onSubmit()'>
+		<div class="field">
+			<div class="control">
+				<input class="input is-primary" type="text" formControlName='uname' placeholder="User name">
+			</div>
+		</div>
+		<div class="field">
+			<div class="control">
+				<input class="input is-primary" type="password" formControlName='password' placeholder="Password">
+			</div>
+		</div>
+		<div class="field">
+			<div class="control">
+				<input class="button is-primary" type="submit" value="login">
+				</div>
+		</div>
+	</form>
+	`,
+	styleUrls: ['../style.css', '../bulma.css']
+})
+export class AdminLogin {
+  title = 'login'
+	loginForm: FormGroup
+
+	constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+		this.loginForm = formBuilder.group({
+			uname: ['', Validators.required],
+			password: ['', Validators.required],
+		})
+	}
+
+	onSubmit() {
+		console.log(this.loginForm)
+		const form = {
+			"uname": this.loginForm.controls.uname.value,
+			"password": this.loginForm.controls.password.value
+		}
+
+		this.http.post('http://home:8080/admin/login/', form)
+		.subscribe(data => console.log(data))
+	}
+}
--- mbs/src/app/app.module.ts
+++ mbs/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./routing";
 import { AppComponent } from "./app.component";
 import { QuestionComponent } from './student/question/question.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
+import { AdminLogin } from './admin/login'
 
 @NgModule({
-  declarations: [AppComponent, QuestionComponent],
-  imports: [BrowserModule, AppRoutingModule],
+	declarations: [
+		AppComponent,
+		QuestionComponent,
+		AdminLogin
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+	],
   providers: [],
   bootstrap: [AppComponent]
 })
