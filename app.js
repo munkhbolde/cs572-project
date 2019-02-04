aa69a9dbc4c9260f4d7865630437de0ded09cc7a
Munkhbold Enkhtur <apple.muugii@gmail.com>
2019-02-04 12:49:10 -0600

    removing console.log

--- app.js
+++ app.js
@@ -42,7 +42,6 @@ check_token = function (req, res, next) {
 
   const token = req.headers.authorization.split(' ')[1]
   jwt.verify(token, 'secret', function(err, decoded) {
-    console.log(decoded)
     if (decoded.type !== 'admin')
       res.render('403', { status: 403, url: req.url });
   })
