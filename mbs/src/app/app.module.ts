2bc7d1b2388f512fe17d96d7989601d399289527
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-02-03 21:24:48 -0600

    [MODULE.TS]

--- mbs/src/app/app.module.ts
+++ mbs/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./routing";
 import { AppComponent } from "./app.component";
-import { QuestionComponent } from './student/question/question.component';
+import { QuestionComponent } from "./student/question/question.component";
+import { AceEditorModule } from "ng2-ace-editor";
 
 @NgModule({
   declarations: [AppComponent, QuestionComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AceEditorModule],
   providers: [],
   bootstrap: [AppComponent]
 })
