61cf19b33b75dd32ae4252d39109d5a45d22289a
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-02-03 20:40:27 -0600

    question component created as blank

--- mbs/src/app/app.module.ts
+++ mbs/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./routing";
 import { AppComponent } from "./app.component";
+import { QuestionComponent } from './student/question/question.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QuestionComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
