7a723a82738c47129132ba08407a652186120f48
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-02-04 10:45:26 -0600

    [FIX] merge on app.module.ts

--- mbs/src/app/app.module.ts
+++ mbs/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./routing";
 import { AppComponent } from "./app.component";
-<<<<<<< HEAD
 import { QuestionComponent } from "./student/question/question.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AdminLogin } from "./admin/login";
+import { StaffCreate } from "./admin/create-staff";
 import { AceEditorModule } from "ng2-ace-editor";
 
 @NgModule({
-  declarations: [AppComponent, QuestionComponent, AdminLogin],
+  declarations: [AppComponent, QuestionComponent, AdminLogin, StaffCreate],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -19,29 +19,6 @@ import { AceEditorModule } from "ng2-ace-editor";
     HttpClientModule,
     AceEditorModule
   ],
-=======
-import { QuestionComponent } from './student/question/question.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http'
-import { AdminLogin } from './admin/login'
-import { StaffCreate } from './admin/create-staff'
-import { AceEditorModule } from "ng2-ace-editor";
-
-@NgModule({
-	declarations: [
-		AppComponent,
-		QuestionComponent,
-		AdminLogin,
-		StaffCreate
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		ReactiveFormsModule,
-		HttpClientModule,
-		AceEditorModule
-	],
->>>>>>> 55ffd9a35fa2d2257224b496b670d68218cdbf02
   providers: [],
   bootstrap: [AppComponent]
 })
