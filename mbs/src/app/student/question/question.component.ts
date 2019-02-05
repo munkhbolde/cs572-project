f426e714eda282c5129d106c747af034a924ce95
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-02-05 14:22:38 -0600

    Solution changed on ACE editor

--- mbs/src/app/student/question/question.component.ts
+++ mbs/src/app/student/question/question.component.ts
@@ -1,35 +1,60 @@
 import { Component, OnInit, Input, ViewChild } from "@angular/core";
-import { AuthService } from "../../services/auth.service";
-
 import "ace-builds/webpack-resolver";
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: "app-question",
   templateUrl: "./question.component.html",
   styleUrls: ["./question.component.css"]
 })
 export class QuestionComponent implements OnInit {
-  @ViewChild("editor") editor;
-  number: number;
-  question: string;
-  text: string;
-
+  private exam = {
+    question: "",
+    anwser: "",
+    snapshot: []
+  };
   options: any = { maxLines: 1000, printMargin: false };
   private snapshot: string[] = [];
+  private code: string = "";
 
-  constructor(private auth: AuthService) {
-    this.question =
-      "1. Write a function that remove only even elements from integer array";
+  constructor() {
+
   }
 
   ngOnInit() {
-    this.auth.login();
+
   }
 
   onChange(code: string) {
+    console.log(code);
     this.snapshot.push(code);
+    this.code = code;
   }
 
   submitAnswer() {
     console.log(this.snapshot);
+
+    if (localStorage["exam"] != null) {
+      //Insert data
+      console.log(localStorage["exam"]);
+    } else {
+      //TODO: Guard here no submission
+    }
+  }
+
+  nextQuestion() {
+    if (localStorage.getItem("exam") === null) {
+      this.exam.anwser = this.code;
+      // this.exam.question = this.code // TODO : getQuestion
+      this.exam.snapshot = this.snapshot;
+      localStorage.setItem("exam", JSON.stringify(this.exam));
+    } else {
+      localStorage["exam"].concat(",", JSON.stringify(this.exam));
+    }
+
+    this.exam.anwser = null;
+    this.exam.question = null;
+    this.exam.snapshot = null;
+    this.code = "";
+    this.snapshot = [];
   }
 }
